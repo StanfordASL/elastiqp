86dda129217caefa57f4a0a3979620bacb08c662
Daniel Morton <danielpmorton@gmail.com>
2026-09-21 21:52:57 -0700

    Cleanup elastiqp comments

--- include/elastiqp/elastiqp.hpp
+++ include/elastiqp/elastiqp.hpp
@@ -1,16 +1,4 @@
-// ElastiQP: an always-feasible QP solver for constrained robot control.
-//
-// Umbrella header. The three backends solve the same elastic QP (stated in
-// elastiqp/common.hpp) and return the same Solution certificate:
-//
-//   elastiqp::das::Solver   DAS:  dual active set (DAQP-style)           das.hpp
-//   elastiqp::pdal::Solver  PDAL: primal-dual augmented Lagrangian       pdal.hpp
-//   elastiqp::ipm::Solver   IPM:  proximal interior-point method (PIQP)  ipm.hpp
-//
-// elastiqp::Solver / Settings / Solve() name the default backend, the
-// active-set method. Each header is self-contained and can be included on
-// its own. relax() and the KKT VJP (kkt_vjp.hpp) are available on the PDAL
-// and IPM backends only.
+// ElastiQP: an always-feasible QP solver for constrained robot control
 
 #pragma once
 
